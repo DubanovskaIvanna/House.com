10c6144bde90f63e475344765c3b827df337f79e
Dubanovska Ivanna <55243824+DubanovskaIvanna@users.noreply.github.com>
2020-05-06 23:14:02 +0300

    Add project House.com

---
+++ proj/index.js
@@ -0,0 +1,37 @@
+
+// var http = require("http");
+
+// http.createServer(function (request, response){
+//     response.writeHead(200,{'Content-Type': 'text/plain'});
+//     response.writeHead(200,{'Content-Type': 'text/html'});
+//     response.end ('<h5>Header from server!</h5>');
+    
+// }).listen(5000);
+// console.log('Server running at http://127.0.0.1:5000/');
+
+// let client = {
+//     name : "string",
+//     surname:"string",
+//     loginIk: "string",
+//     email:"string",
+//     age:24,
+//     type:1,
+// };
+
+// let house = {
+//     nameOfHouse: "dim",
+//     dateOfFree: Date(),
+//     nameO: "str",
+//     pricePerDay: 100
+// };
+
+// let landlord = {
+//     loginO:"string",
+//     nameO:"STRING",
+//     nameOfHouse: "str",
+//     addAppartmen : function (){
+
+//      }
+// };
+
+   
